a34fed7ca870fea54aa02ebbf76e9a550cb9a388
Tushar K <myhome_tushark@yahoo.co.in>
2017-05-02 21:40:55 +0530

    Add files via upload

---
+++ nltk_tagger.py
@@ -0,0 +1,95 @@
+import nltk
+from nltk.corpus import brown
+
+brown_train = brown.tagged_sents(categories='news')
+regexp_tagger = nltk.RegexpTagger(
+    [(r'^-?[0-9]+(.[0-9]+)?$', 'CD'),
+     (r'(-|:|;)$', ':'),
+     (r'\'*$', 'MD'),
+     (r'(The|the|A|a|An|an)$', 'AT'),
+     (r'.*able$', 'JJ'),
+     (r'^[A-Z].*$', 'NNP'),
+     (r'.*ness$', 'NN'),
+     (r'.*ly$', 'RB'),
+     (r'.*s$', 'NNS'),
+     (r'.*ing$', 'VBG'),
+     (r'.*ed$', 'VBD'),
+     (r'.*', 'NN')
+])
+unigram_tagger = nltk.UnigramTagger(brown_train, backoff=regexp_tagger)
+bigram_tagger = nltk.BigramTagger(brown_train, backoff=unigram_tagger)
+
+# This is the semi-cfg
+cfg = {}
+cfg["NNP+NNP"] = "NNP"
+cfg["NN+NN"] = "NNI"
+cfg["NNI+NN"] = "NNI"
+cfg["JJ+JJ"] = "JJ"
+cfg["JJ+NN"] = "NNI"
+
+
+class NPExtractor(object):
+
+    def __init__(self, sentence):
+        self.sentence = sentence
+
+    # Split the sentence into singlw words/tokens
+    def tokenize_sentence(self, sentence):
+        tokens = nltk.word_tokenize(sentence)
+        return tokens
+
+    # Normalize brown corpus' tags ("NN", "NN-PL", "NNS" > "NN")
+    def normalize_tags(self, tagged):
+        n_tagged = []
+        for t in tagged:
+            if t[1] == "NP-TL" or t[1] == "NP":
+                n_tagged.append((t[0], "NNP"))
+                continue
+            if t[1].endswith("-TL"):
+                n_tagged.append((t[0], t[1][:-3]))
+                continue
+            if t[1].endswith("S"):
+                n_tagged.append((t[0], t[1][:-1]))
+                continue
+            n_tagged.append((t[0], t[1]))
+        return n_tagged
+
+    # Extract the main topics from the sentence
+    def extract(self):
+
+        tokens = self.tokenize_sentence(self.sentence)
+        tags = self.normalize_tags(bigram_tagger.tag(tokens))
+
+        merge = True
+        while merge:
+            merge = False
+            for x in range(0, len(tags) - 1):
+                t1 = tags[x]
+                t2 = tags[x + 1]
+                key = "%s+%s" % (t1[1], t2[1])
+                value = cfg.get(key, '')
+                if value:
+                    merge = True
+                    tags.pop(x)
+                    tags.pop(x)
+                    match = "%s %s" % (t1[0], t2[0])
+                    pos = value
+                    tags.insert(x, (match, pos))
+                    break
+
+        matches = []
+        for t in tags:
+            if t[1] == "NNP" or t[1] == "NNI":
+            #if t[1] == "NNP" or t[1] == "NNI" or t[1] == "NN":
+                matches.append(t[0])
+        return matches
+
+
+def main(x):
+    np_extractor = NPExtractor(x)
+    result = np_extractor.extract()
+    print("This hashtags to be generated are: %s" % ", ".join(result))
+
+if __name__ == '__main__':
+    sentence = "Machine learning is the subfield of computer science that, according to Arthur Samuel in 1959, gives computers the ability to learn without being explicitly programmed.[1] Evolved from the study of pattern recognition and computational learning theory in artificial intelligence,[2] machine learning explores the study and construction of algorithms that can learn from and make predictions on data[3] – such algorithms overcome following strictly static program instructions by making data-driven predictions or decisions,[4]:2 through building a model from sample inputs. Machine learning is employed in a range of computing tasks where designing and programming explicit algorithms with good performance is difficult or unfeasible; example applications include email filtering, detection of network intruders or malicious insiders working towards a data breach,[5] optical character recognition (OCR),[6] learning to rank and computer vision."
+    main(sentence)
